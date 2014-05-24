d31d02955dc986ad6febe6326b4c490464f82afc
GermainZ <germanosz@gmail.com>
2014-05-24 19:04:22 +0300

    Add options to select default tab for Instant Mixes, Radio and Explore panes

--- src/com/germainz/gmusiclistenlater/XposedMod.java
+++ src/com/germainz/gmusiclistenlater/XposedMod.java
@@ -2,6 +2,9 @@ package com.germainz.gmusiclistenlater;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
@@ -16,10 +19,32 @@ import static de.robv.android.xposed.XposedHelpers.setObjectField;
 public class XposedMod implements IXposedHookLoadPackage {
 
     private static final XSharedPreferences PREFS = new XSharedPreferences("com.germainz.gmusiclistenlater");
-    private static final String PREF_DEFAULT_PANE= "pref_default_pane";
-    private static final String PREF_DEFAULT_LIBRARY_TAB= "pref_library_default_tab";
-    private static final String DEFAULT_PANE= "MY_LIBRARY";
-    private static final String DEFAULT_LIBRARY_TAB = "ARTISTS";
+    private static final String PREF_DEFAULT_PANE = "pref_default_pane";
+    private static final String DEFAULT_PANE = "MY_LIBRARY";
+
+    private static final Map<String, ScreenFragment> FRAGMENTS;
+
+    private static final class ScreenFragment {
+        String prefDefaultTab;
+        String defaultTab;
+
+        public ScreenFragment(String prefDefaultTab, String defaultTab) {
+            this.prefDefaultTab = prefDefaultTab;
+            this.defaultTab = defaultTab;
+        }
+    }
+
+    static {
+        FRAGMENTS = new HashMap<String, ScreenFragment>();
+        FRAGMENTS.put("com.google.android.music.ui.MyLibraryFragment",
+                new ScreenFragment("pref_library_default_tab", "artists"));
+        FRAGMENTS.put("com.google.android.music.ui.InstantMixesFragment",
+                new ScreenFragment("pref_instant_mixes_default_tab", "instant_mixes"));
+        FRAGMENTS.put("com.google.android.music.ui.RadioFragment",
+                new ScreenFragment("pref_radio_default_tab", "stations"));
+        FRAGMENTS.put("com.google.android.music.ui.ExploreFragment",
+                new ScreenFragment("pref_explore_default_tab", "top_charts"));
+    }
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
@@ -39,17 +64,19 @@ public class XposedMod implements IXposedHookLoadPackage {
                 }
         );
 
-        findAndHookConstructor("com.google.android.music.ui.MyLibraryFragment", lpparam.classLoader,
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        PREFS.reload();
-                        setObjectField(param.thisObject, "mDefaultTab",
-                                PREFS.getString(PREF_DEFAULT_LIBRARY_TAB, DEFAULT_LIBRARY_TAB));
+        for (String className : FRAGMENTS.keySet()) {
+            final ScreenFragment screenFragment = FRAGMENTS.get(className);
+            findAndHookConstructor(className, lpparam.classLoader,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            PREFS.reload();
+                            setObjectField(param.thisObject, "mDefaultTab",
+                                    PREFS.getString(screenFragment.prefDefaultTab, screenFragment.defaultTab));
+                        }
                     }
-                }
-        );
-
+            );
+        }
 
     }
 }
